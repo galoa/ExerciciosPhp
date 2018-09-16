5112f6e2a312b2d9557d9e4cc220134f4d87228e
ChristianRicardo <43251424+ChristianRicardo@users.noreply.github.com>
2018-09-16 14:05:35 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -11,6 +11,31 @@ class Resolucao implements TextWrapInterface {
    * {@inheritdoc}
    */
   public function textWrap(string $text, int $length): array {
+    
+    	$words=explode("", $text);
+  	$limite=$length;
+  	foreach($words as $word){
+  		$limite=strlen($word);
+
+  			if($limite>=0){
+  				echo $word." ";
+  				$limite--;
+  			}
+
+  			else{
+  				if(strlen($word)>$length){
+  					echo substr($word, 0, $length-1). " ";
+  					echo "<br>";
+  					$limite=$length;
+  					echo substr($word, $length-1, -1);
+  					$limite -= strlen(substr($word, $length-1, -1));
+  				}
+  				else{
+  					echo"<br>$word";
+  					$limite=$length;
+  				}
+  			}
+  	}
     return [""];
   }
 
