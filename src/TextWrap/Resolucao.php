591afb6440792e5f8ae480b16982e5d69e215907
ChristianRicardo <43251424+ChristianRicardo@users.noreply.github.com>
2018-09-20 02:11:24 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -4,47 +4,32 @@ namespace Galoa\ExerciciosPhp\TextWrap;
  * Implemente sua resolução aqui.
  */
 class Resolucao implements TextWrapInterface {
+	public function textWrap(string $text, int $length){
 
-  public function textWrap(string $text, int $length): array {
-		//variaveis locais
-		$words = explode(' ',$text);//separa o texto em um array de palavras
-		$vet = array();//array que será retornado 
-		$string = "";//string manipuladora vazia
-		$limite = $length; // limite de caracter 
-		$line = 0;//linha do array
-		for($i = 0; $i < count($words); $i++){
-			$string = $words[$i].' ';
-			if(( strlen($words[$i]) > $length)){
-				$this->cutWord($vet,$words[$i],$limite,$length,$line);//chama a função de corte de palavra, para inserir o restante na próx linha
-			}else
-				if( $limite >= strlen($string)){	
-					$vet[$line] = (array_key_exists($line,$vet) )? $vet[$line].$string : $string;//add a palavra na linha do vetor
-					$limite -= strlen($string);// subtração para saber quantos caracteres faltam 
-				}else 
-					if($limite < strlen($string)){
-						$line++;//incrementa o valor de linha para a string ser adicionado no outro indice do array
-						$limite = $length;//limite recebe o valor de inicio, para não gerar sobrecarga
-						$vet[$line] = $string;// add a palavra na linha do vetor
-						$limite -= strlen($string);//subtração para saber quantos caracteres faltam
-					}
-		}
-    return $vet;
-  }
-  
-  private function cutWord(&$array,$words,&$limite,$length,$index){
+  $words=explode(" ", $text); 
+    $limite=$length; 
+    $arrayreturn=array(); 
+	$string=$words;
+
+
+foreach($words as $word){
+	$limite-=strlen($word);
 	
-	  for($i = 0; $i < strlen($words); $i++){
-		if( ($limite != $length) && ($i == 0) ){//verifica se o indice está sem palavras
-			$index++; //soma+1 no vetor
-			$limite = $length;//limite recebe o valor de inicio	
-		}		
-		if($limite <= 0) {//verifica se limite >0		 
-			$index++;//soma +1 à linha
-			$limite = $length;//limite recebe o valor de inicio	
-		}
-		$array[$index] = ( array_key_exists($indice,$array) )? $array[$indice].$palavra[$i] : $palavra[$i];
+	if($limite>=0){
+		echo $word." ";
 		$limite--;
-	  }
-	  $array[$index] = $array[$index].' ';
-  }
-} 
+	}else {
+		if(strlen($word)>$length){
+			echo substr($word,0,$length-1)."-";
+			echo "<br>";
+			$limite=$length;
+			echo substr($word, $length-1, -1);
+			$limite-=strlen(substr($word, $length-1, -1));
+	}else{
+			echo"<br>$word";
+			$limite=$length;}
+		
+	}
+}
+	}
+}
