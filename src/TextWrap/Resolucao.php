92c075700f4914d7e87239b9cc3f48885f351fda
Everton Vieira <everton@versatecnologia.com.br>
2018-11-27 15:19:31 -0200

    Adiciona metodo wordDivide a Class TextWrapInterface
    Metodo responsavel em separar as palavras contidas na string text retornando um vetor

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -14,4 +14,26 @@ class Resolucao implements TextWrapInterface {
     return [""];
   }
 
+	// separa um texto em varias palavras, equivalente a função explode() do php
+   public function wordDivide(string $text):array{
+        $palavras = array();
+        $num_palavra = (int) 0;
+        $tam_text = (int) strlen($text);
+        $palavras[$num_palavra] = (string) "";
+
+        for ($i = (int) 0; $i < $tam_text; $i++) {
+            //caso encontre um espaço ou uma quebra de linha
+            if($text[$i] == ' ' || ord($text[$i]) == 10){
+                if ($palavras[$num_palavra] != "") {
+                    $num_palavra++;
+                }
+                $palavras[$num_palavra] = (string) "";
+            } else {
+                $palavras[$num_palavra] = $palavras[$num_palavra] . $text[$i];
+            }
+
+        }
+        return $palavras;
+    }
+
 }
