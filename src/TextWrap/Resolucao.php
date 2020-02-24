9e5d6bc278cf2af4f6c64e48676516a95ba82ec2
Henrique <henrique9811@hotmail.com>
2020-02-24 08:45:20 -0300

    Implementação da Resolução feita

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -11,7 +11,41 @@ class Resolucao implements TextWrapInterface {
    * {@inheritdoc}
    */
   public function textWrap(string $text, int $length): array {
-    return [""];
+      if(strlen($text) == 0) 
+      return [""];
+     
+     
+     $linha = "";
+     $caraUsados = 0;
+     $palavras = explode(" ",$text);
+     $ret = [];
+
+     foreach($palavras as $palavra){
+       if($caraUsados + strlen($palavra) < $length){
+           if($linha != "")
+            $linha .= " ";
+           $linha .= $palavra;
+           $caraUsados = strlen($linha);
+       }
+       else{
+         array_push($ret,$linha);
+         $linha = "";
+         $caraUsados = 0;
+         if($palavra <= $length){
+           $linha .= $palavra;
+           $caraUsados = strlen($palavra);
+         }
+         else{
+           array_push($ret,substr($linha,0,$length));
+           $corte = strlen($palavra) - $length;
+           $linha = substr($palavra,-1,$corte);
+           $caraUsados = strlen($linha);
+         }
+       }
+     }
+     if($linha != "")
+     array_push($ret,$linha);
+     return $ret;
   }
 
 }
