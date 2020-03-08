bdbab5169f78237a9f0ae4fc41ec92893e2edb7e
Lucas de Mello <lucas_mello6@hotmail.com>
2020-03-08 20:22:11 -0300

    line break algorithm implemented; Class attributes were used to facilitate the exchange of context between methods

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -8,10 +8,74 @@ namespace Galoa\ExerciciosPhp\TextWrap;
 class Resolucao implements TextWrapInterface {
 
   /**
+   * O texto que será utilizado como entrada.
+   */
+  private $text = "";
+
+  /**
+   * Em quantos caracteres a linha deverá ser quebrada.
+   */
+  private $length = 0;
+
+  /**
+   * Linhas geradas pela quebra de linhas.
+   */
+  private $lines = [];
+
+  /**
+   * Quebra de palavras maiores que o length.
+   *
+   * @param string $word
+   *   A palavra que será utilizado como entrada.
+   *
+   * @return string
+   *   Retorno da ultima sub palavra.
+   */
+  private function wordBreak(string $word): string {
+    $splitWord = str_split($word);
+    $subWord = "";
+   
+    foreach($splitWord as $chart) {
+      $subWord = $subWord . $chart;
+
+      if(mb_strlen($subWord, 'utf-8') == $this->length) {
+        array_push($this->lines, $subWord);
+        $subWord = "";
+      }
+    }
+
+    return $subWord;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function textWrap(string $text, int $length): array {
-    return [""];
-  }
+    $this->text = $text;
+    $this->length = $length;
 
+    $words = explode(' ', $this->text);
+    $line = "";
+
+    foreach ($words as $index => $word) {
+      if ($line != "")
+        $attemptFit = $line . " ";
+      $attemptFit = $attemptFit . $word;
+
+      if (mb_strlen($attemptFit, 'utf-8') <= $this->length)
+        $line = $attemptFit;
+      else {  
+        array_push($this->lines, $line);
+
+        $line = $word;
+
+        if (mb_strlen($word, 'utf-8') > $this->length)
+          $line = $this->wordBreak($word);
+      }
+    };
+
+    array_push($this->lines, $line);
+
+    return $this->lines;
+  }
 }
