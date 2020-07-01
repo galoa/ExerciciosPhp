ebe8dce139b78b5905783b9ba515b4debacfec78
Math8bit <67442630+Math8bit@users.noreply.github.com>
2020-07-01 20:16:39 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -10,8 +10,57 @@ class Resolucao implements TextWrapInterface {
   /**
    * {@inheritdoc}
    */
-  public function textWrap(string $text, int $length): array {
-    return [""];
-  }
 
+
+  public function textWrap(string $text, int $length): array {
+
+  	//texto auxiliar que vai servir para trasnformar o texto em array.
+  	$aux_text = preg_split('/ /', $text, -1, PREG_SPLIT_NO_EMPTY);  	
+  	//variável auxiliar para armazenar as palavras que serão adicionadas no laço de repetição.
+  	$aux = "";
+  	//array responsável por guardar toda o resultado da função, já com as divisões.
+  	$array = array();
+  	
+
+  if(is_array($aux_text) || is_object($words)){
+  	foreach ($aux_text as $words) {
+  		$wordLength = strlen($words);
+  		$auxLength = strlen($aux);
+  		if($wordLength + 1 + $auxLength <= $length){
+  			if(auxLength != 0){
+  				$aux = $aux . " " . $words;
+  			}
+  			else
+  				$aux = $words;
+  		}
+  		else{
+  			if(wordLength <= $length){
+  				array_push($array, $aux);
+  				$aux = $words;
+  			}
+
+  			if($wordLength > $length){
+  				$leftOver = str_split($words,$length);
+
+  				foreach ($leftOver as $lefts) {
+  					$leftsLength = strlen($lefts);
+  					if($leftsLength == $length)
+  						array_push($array, $lefts);
+  					else
+  						$aux = $aux . $lefts;	
+  				}
+  			}
+  		}
+  	}
+  }
+  	if(strlen($aux)!=0)
+  		array_push($array, $aux);
+  	
+
+
+
+
+  	
+    return $array;
+  }
 }
