4676f3ca15f47e31ee5d4476ef36c3f5d059fdd2
Marcello Simoes <marcello.ssf@outlook.com>
2020-07-15 04:11:06 -0300

    Exercicio proposto resolvido.

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -5,13 +5,90 @@ namespace Galoa\ExerciciosPhp\TextWrap;
 /**
  * Implemente sua resolução aqui.
  */
-class Resolucao implements TextWrapInterface {
+class Resolucao implements TextWrapInterface
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  public function textWrap(string $text, int $length): array {
-    return [""];
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function textWrap(string $text, int $length): array
+    {
+        if ($text == "") {
+            return [""];
+        }
 
+
+        //Separa o texto em palavras
+
+        $palavrasDoTexto = preg_split('/ /', $text);
+
+        $linha = '';
+        $textoFormatado = [];
+
+
+        foreach ($palavrasDoTexto as $palavra) {
+            $tamanhoDaPalavra = strlen($palavra);
+            $tamanhoDaLinha = strlen($linha);
+
+            //Testa se a palavra cabe na linha
+            if ($tamanhoDaPalavra + $tamanhoDaLinha < $length) {
+                if ($tamanhoDaLinha == 0) {
+                    $linha = $palavra;
+                } else {
+                    $linha .= " $palavra";
+                }
+
+                //Testar o por que a palavra não coube na linha
+            } else {
+                //Testa se a palavra está dentro do limite de caracteres
+                if ($tamanhoDaPalavra <= $length) {
+                    array_push($textoFormatado, $linha);
+                    $linha = $palavra;
+
+                    //Palavra excedeu o numero de caracteres
+                } else {
+
+                    if ($tamanhoDaLinha != 0) {
+                        $sobraDaLinha = $length - ($tamanhoDaLinha + strlen(' '));
+                    } else {
+                        $sobraDaLinha = $length;
+                    }
+
+                    if ($sobraDaLinha > 0) {
+                        if ($tamanhoDaLinha != 0) {
+                            $linha = $linha . ' ' . substr($palavra, 0, $sobraDaLinha);
+                        } else {
+                            $linha = $linha . substr($palavra, 0, $sobraDaLinha);
+                        }
+                    }
+
+                    array_push($textoFormatado, $linha);
+                    $linha = '';
+
+
+                    $sobraDaPalavra = substr($palavra, $sobraDaLinha);
+                    $separarSobraDaPalavra = str_split($sobraDaPalavra, $length);
+
+                    foreach ($separarSobraDaPalavra as $pedaco) {
+                        if (strlen($pedaco) == $length) {
+                            array_push($textoFormatado, $pedaco);
+                        } else {
+                            $linha = $linha . $pedaco;
+                        }
+                    }
+                }
+                
+                
+                }
+
+
+               
+            }
+            //Coloca no texto formatado a ultima linha
+            if (strlen($linha) != 0) {
+                array_push($textoFormatado, $linha);
+            }
+
+            return $textoFormatado;
+    } 
 }
