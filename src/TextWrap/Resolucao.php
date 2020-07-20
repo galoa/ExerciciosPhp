e4831e1597889f7368c1aa146d527c541fcda56c
Math8bit <67442630+Math8bit@users.noreply.github.com>
2020-07-20 00:11:34 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -14,24 +14,39 @@ class Resolucao implements TextWrapInterface {
 
   public function textWrap(string $text, int $length): array {
  
-    
-  	if($text == null)
-  		print("");
-  	if(length($length) <= strlen($text)){
-  		return $text;
-  	} else if (length($length) < strlen($text)) {
-  		$literalArray = array (
-  			str_split($text, $length);
-  		);	
+    $wrap = array [];
+  		$literalAux = "";
+  		$sizeAux = strlen($aux);
+  		$literalArray = explode( ' ',$text);
 
-  		return $literalArray;	
-  	}
-  
- 
-  	
+  		if($text == "")
+  			print("Texto vazio");
 
+  		foreach ($literalArray as $words) {
+  			$sizeWords = strlen($words);
+  			if($sizeWords + $sizeAux + 1 < $length){
+  				if($sizeAux == 0)
+  					$aux = $words;
+  				else
+  					$aux += ' '.$words;
+  			}else{
+  				if($sizeWords <= $length){
+  					$aux = $words;
+  					array_push($wrap,$aux);
+  				}else{
+  					$wrapText = str_split($text,$length);
+  					foreach ($wrapText as $wrapChar) {
+  						$sizeWrapChar = strlen($wrapChar);
+  						if($sizeWrapChar == $length)
+  							array_push($wrap, $wrapChar);
+  						else
+  							$aux += $wrapChar;
+  					}
+  				}
+  			}
+  		}
 
-  	
-    
+  		return $wrap;  	
+
   }
 }
