d51f1eb03f8639c6a6269a868d20c58d6e724f9a
Henrique Silva <henrique9811@hotmail.com>
2020-08-10 20:34:13 -0300

    Explicação do código em comentarios

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -24,41 +24,41 @@ class Resolucao implements TextWrapInterface {
    * testes unitários.
    */
   public function textWrap(string $text, int $length): array {
-    if(strlen($text) == 0) 
+    if(strlen($text) == 0) // verificação se a String passada está vaiza, se sim retorna um Array vazio
       return [""];
      
+     /*inicialização das variaveis a serem usadas para a separação da String*/ 
+     $linha = ""; //essa variavel armazena as palavras ou partes dela antes de guardar no array de retorno
+     $caraUsados = 0; //essa varivavel conta quantos caracteres já foram usados
+     $palavras = explode(" ",$text);// criação de um array contendo as palavras da String original
+     $ret = [];// array de retorno contendo todas as linhas que foram criadas
      
-     $linha = "";
-     $caraUsados = 0;
-     $palavras = explode(" ",$text);
-     $ret = [];
-
-     foreach($palavras as $palavra){
-       if($caraUsados + strlen($palavra) < $length){
-           if($linha != "")
+     foreach($palavras as $palavra){//foreach utilizado para iterar o array das palavras da String original
+       if($caraUsados + strlen($palavra) < $length){//verificação se o numero de caracteres usados mais o dos carcteres da proxima palavra não superem o length passado da linha
+           if($linha != "")//verifica se a linha não está em branco se não estiver coloca um espaço em branco na linha
             $linha .= " ";
-           $linha .= $palavra;
-           $caraUsados = strlen($linha);
+           $linha .= $palavra;//adciona a palavra a linha
+           $caraUsados = strlen($linha);// substitui os caracteres usados pelo numero de caracteres da linha 
        }
-       else{
-         array_push($ret,$linha);
-         $linha = "";
-         $caraUsados = 0;
-         if($palavra <= $length){
-           $linha .= $palavra;
+       else{ //este else cobre se a proxima palavra não cabe na linha
+         array_push($ret,$linha);//adciona a linha no array de retorno
+         $linha = "";// cria uma nova linha
+         $caraUsados = 0;// autualiza o caracteres usados
+         if($palavra <= $length){// verifica se a palavra é menor que a linha especificada, se sim adciona ela na linha e
+           $linha .= $palavra; // atualiza os caracteres usados
            $caraUsados = strlen($palavra);
          }
-         else{
-           array_push($ret,substr($linha,0,$length));
-           $corte = strlen($palavra) - $length;
-           $linha = substr($palavra,-1,$corte);
-           $caraUsados = strlen($linha);
+         else{ // este else cobre o segundo caso, se a palavra for maior do que a linha especificada
+           array_push($ret,substr($linha,0,$length));// coloca a parte da palavra na linha criando uma substring dela do tamanho da linha
+           $corte = strlen($palavra) - $length; // cria o numero de caracteres para o corte da palavra
+           $linha = substr($palavra,-1,$corte); // cria a linha com o corte da palavra maior que a linha
+           $caraUsados = strlen($linha);// atualiza os catacteres usados
          }
        }
      }
-     if($linha != "")
-     array_push($ret,$linha);
-     return $ret;
+     if($linha != "") // após o for verifica se ainda existe algo na linha, se sim coloca ela no array de retorno
+     array_push($ret,$linha);// este ultimo if foi feito pois geralmente sobrava uma ultima linha que não entrava no array
+     return $ret;// retorna o array contendo as linhas
   }
 
 }
