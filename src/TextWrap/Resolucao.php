0c19f546a86b2e33d4c5ac691dd5ad26a923db3c
Alexandre <asilva009@yahoo.com.br>
2020-08-13 14:17:07 -0300

    resolução do exercício backend

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -23,46 +23,45 @@ class Resolucao implements TextWrapInterface {
    * nós colocamos esse mock para poder rodar a análise de cobertura dos
    * testes unitários.
    */
-  public function textWrap(string $text, int $length): array {
-    if ($length === 8) {
-      return [
-        'Se vi',
-        'mais',
-        'longe',
-        'foi por',
-        'estar de',
-        'pé',
-        'sobre',
-        'ombros',
-        'de',
-        'gigantes',
-      ];
+  function textWrap(string $text, int $length): array {
+    $resultado = []; // array com resultado desejado
+    $caracter = []; // array da string de entrada
+    $posicaoCaracter = 1; //posição do caracter na linha
+    $posicao = 0; // posição das linhas do array resultado
+    $resultado[$posicao] = ''; // cada linha do array resultado
+    
+    // loop para criar o array da string
+    for ($i = 0; $i < strlen($text); $i++) {
+        $caracter[$posicaoCaracter] = $text[$i];
+        $posicaoCaracter++;      
+        // verifica se a linha já atingiu o tamanho estipulado  
+        if (($posicaoCaracter - 1) == $length) {
+            // verifica se a linha tem espaços entre as palavras
+            if (in_array(' ', $caracter)) {
+                // verifica se o próximo caracter é ' '
+                if (isset($text[$i + 1]) && $text[$i + 1] != ' ') {
+                    // elimina ' ' da linha evitando corte de palavras
+                    for ($e = count($caracter); $e > 0; $e--) {
+                        if ($caracter[$e] == ' ') {
+                            $i -= $length - $e;
+                            array_pop($caracter);
+                            break;
+                        }    
+                        array_pop($caracter);     
+                    }
+                }              
+            }
+            // gera linha
+            $resultado[$posicao] = implode($caracter);
+            // prepara variáveis para iniciar uma nova linha
+            $posicaoCaracter = 1;
+            $caracter = [];
+            $posicao++;
+        // construção da última linha
+        }elseif ($i == (strlen($text)) - 1) {
+            $resultado[$posicao] = implode($caracter);                 
+        }
     }
-    elseif ($length === 12) {
-      return [
-        'Se vi mais',
-        'longe foi',
-        'por estar de',
-        'pé sobre',
-        'ombros de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 10) {
-      // Por favor, não implemente o código desse jeito, isso é só um mock.
-      $ret = [
-        'Se vi mais',
-        'longe foi',
-        'por estar',
-        'de pé',
-        'sobre',
-      ];
-      $ret[] = 'ombros de';
-      $ret[] = 'gigantes';
-      return $ret;
-    }
-
-    return [""];
-  }
-
+    return $resultado;
+  } 
 }
