71064f3ec298c8d68de86224591356dc56e985a6
TiagoSilvaADS <54046968+TiagoSilvaADS@users.noreply.github.com>
2020-08-15 13:47:14 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -2,28 +2,42 @@
 
 namespace Galoa\ExerciciosPhp\TextWrap;
 
-
+/**
+ * Implemente sua resolução nessa classe.
+ *
+ * Depois disso:
+ * - Crie um PR no github com seu código
+ * - Veja o resultado da correção automática do seu código
+ * - Commit até os testes passarem
+ * - Passou tudo, melhore a cobertura dos testes
+ * - Ficou satisfeito, envie seu exercício para a gente! <3
+ *
+ * Boa sorte :D
+ */
 class Resolucao implements TextWrapInterface {
 
-  
+  /**
+   * {@inheritdoc}
+   *
+   * Apague o conteúdo do método abaixo e escreva sua própria implementação,
+   * nós colocamos esse mock para poder rodar a análise de cobertura dos
+   * testes unitários.
+   */
   public  function textWrap(string $text, int $length): array {
     
-     // Armazenando a quantidade maxima de caracteres que foi passada
+    // Armazenando a quantidade maxima de caracteres que foi passada.
 
-     $max = $length;
-
+    $max = $length;
     $novoTexto = "";
-    
     $totalLength = 0;
-
-    // Dividindo a string em string menores
+    
+    // Dividindo a string em string menores.
 
     $texto = explode(" ", $text);
     
-
     foreach($texto as $string) {
 
-      // Adicionando os Espacos que foram removidos no explode
+      // Adicionando os Espacos que foram removidos no explode.
        $string .= " ";
     
         if ($totalLength + strlen($string) <= $max) {
@@ -39,8 +53,8 @@ class Resolucao implements TextWrapInterface {
           $totalLength = strlen($string);
         } 
       }
-        /* basicamente ele verifica o tamanho da linha a cada iteração
-           se exceder ele adiciona uma quebra de linha ao texto e reseta essa variável q guarda o tamanho da linha
+        /* basicamente ele verifica o tamanho da linha a cada iteração,
+           se exceder ele adiciona uma quebra de linha ao texto e reseta essa variável q guarda o tamanho da linha.
         */
           echo $novoTexto;
     
