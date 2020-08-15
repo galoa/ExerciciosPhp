9a272ae3ca8672c7918d4bb21e4da5692de40abb
TiagoSilvaADS <54046968+TiagoSilvaADS@users.noreply.github.com>
2020-08-15 17:59:56 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -45,7 +45,8 @@ class Resolucao implements TextWrapInterface {
     // Basicamente ele verifica o tamanho da linha a cada iteração,se exceder.
     // Ele adiciona uma quebra de linha ao texto e reseta essa.
     // Variável que guarda o tamanho da linha.
-    return $novoTexto;
+    echo $novoTexto;
+    return [" "];
   }
 
 }
