0e50ceca86d1b3026faccd24e7fbda290521edb1
TiagoSilvaADS <54046968+TiagoSilvaADS@users.noreply.github.com>
2020-08-15 13:22:41 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -2,60 +2,48 @@
 
 namespace Galoa\ExerciciosPhp\TextWrap;
 
-/**
- * Implemente sua resolução nessa classe.
- *
- * Depois disso:
- * - Crie um PR no github com seu código
- * - Veja o resultado da correção automática do seu código
- * - Commit até os testes passarem
- * - Passou tudo, melhore a cobertura dos testes
- * - Ficou satisfeito, envie seu exercício para a gente! <3
- *
- * Boa sorte :D
- */
+
 class Resolucao implements TextWrapInterface {
 
-  /**
-   * {@inheritdoc}
-   *
-   * Apague o conteúdo do método abaixo e escreva sua própria implementação,
-   * nós colocamos esse mock para poder rodar a análise de cobertura dos
-   * testes unitários.
-   */
-  public function textWrap(string $text, int $length): array {
-       //Dividindo a string em string menores
-        $texto = explode(' ', $text);
+  
+  public  function textWrap(string $text, int $length): array {
     
-    //Armazenando a quantidade maxima de caracteres que foi passada
-    $max = $length;
+     // Armazenando a quantidade maxima de caracteres que foi passada
+
+     $max = $length;
 
     $novoTexto = "";
     
     $totalLength = 0;
+
+    // Dividindo a string em string menores
+
+    $texto = explode(" ", $text);
     
-    //Adicionando os Espacos que foram removidos no explode
 
     foreach($texto as $string) {
-      $string .= " ";
-    
-      if ($totalLength + strlen($string) <= $max) {
-        $totalLength += strlen($string);
-    
-        $novoTexto .= $string;
-      } else {
-        $novoTexto .= "<br/>" . $string;
-    
-        $totalLength = strlen($string);
-      }
-    }
-    /*basicamente ele verifica o tamanho da linha a cada iteração
-     se exceder ele adiciona uma quebra de linha ao texto e reseta essa variável q guarda o tamanho da linha
-     */
-    echo $novoTexto;
-    
-    
-    return [""];
-  }
 
+      // Adicionando os Espacos que foram removidos no explode
+       $string .= " ";
+    
+        if ($totalLength + strlen($string) <= $max) {
+            
+            $totalLength += strlen($string);
+    
+            $novoTexto .= $string;
+
+        } else {
+            
+          $novoTexto .= "<br/>" . $string;
+    
+          $totalLength = strlen($string);
+        } 
+      }
+        /* basicamente ele verifica o tamanho da linha a cada iteração
+           se exceder ele adiciona uma quebra de linha ao texto e reseta essa variável q guarda o tamanho da linha
+        */
+          echo $novoTexto;
+    
+    return [""]; 
+}
 }
