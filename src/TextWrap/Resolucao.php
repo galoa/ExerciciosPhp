26cc95f8ffbc5164dbd99be016b95724284f154d
TiagoSilvaADS <54046968+TiagoSilvaADS@users.noreply.github.com>
2020-08-18 12:49:50 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -23,30 +23,37 @@ class Resolucao implements TextWrapInterface {
    * nós colocamos esse mock para poder rodar a análise de cobertura dos
    * testes unitários.
    */
-  public  function textWrap(string $text, int $length): array {
+    public  function textWrap(string $text, int $length): array {
+    // Variaveis que serao usadas no programa.
     $array = explode(' ', $text);
-    $newText = "";
+    // Divide o texto base em texto menores. 
+    $newText = ""; 
     $rowLength = 0;
     foreach ($array as $string) {
       if (mb_strlen($string, 'UTF-8') + $rowLength <= $length) {
+        // Se a palavra for menor que o maximo de caracteres.
         $string .= " ";
+        // Devolvendo os espacamentos 
         $rowLength += mb_strlen($string, 'UTF-8');
+        // Tamanho atual da linha e incrementado com o tamanho da palvra. 
         $newText .= $string;
-        continue;
       }
       else {
+        // Caso a palavra seja maior que o maximo de caracteres.
         $string .= " ";
         $rowLength = mb_strlen($string, 'UTF-8');
         $newText .= "\n";
         $newText .= $string;
-        continue;
       }
 
     }
+    // FORMATANDO A SAIDA DE DADOS !
     $trimmed = trim($newText, " ");
+    // Retira os espaços em branco do inicio e do final da String.
     $x = explode("\n", $trimmed);
-
+    // Retirando as quebras de linha 
     $clean = array_map('trim', $x);
+    // Remove os espaços em branco de  todos os elementos do array.
 
     return $clean;
   }
