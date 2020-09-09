b6256290e155962264a426ca000a0dcc825f3e33
asilva009 <asilva009@yahoo.com.br>
2020-09-09 14:32:18 -0300

    mais correções

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -24,47 +24,47 @@ class Resolucao implements TextWrapInterface {
    * testes unitários.
    */
   public function textWrap(string $text, int $length): array {
-    $resultado = [];  // Array com resultado desejado.
-    $caracter = []; // Array da string de entrada.
-    $posicaoCaracter = 1; // Posição do caracter na linha.
-    $posicao = 0; // Posição das linhas do array resultado.
-    $resultado[$posicao] = ''; // Cada linha do array resultado.    
+    $resultado = [];  
+    $caracter = []; 
+    $posicaoCaracter = 1; 
+    $posicao = 0; 
+    $resultado[$posicao] = '';
     // Loop para criar o array da string.
     for ($i = 0; $i < strlen($text); $i++) {
       $caracter[$posicaoCaracter] = $text[$i];
-      $posicaoCaracter++;      
-      // Verifica se a linha já atingiu o tamanho estipulado.  
+      $posicaoCaracter++;
+      // Verifica se a linha já atingiu o tamanho estipulado.
       if (($posicaoCaracter - 1) == $length) {
-          // Verifica se a linha tem espaços entre as palavras.
-          if (in_array(' ', $caracter)) {
-              // Verifica se o próximo caracter é ' '.
-              if (isset($text[$i + 1]) && $text[$i + 1] != ' ') {
-                  // Elimina ' ' da linha evitando corte de palavras.
-                  for ($e = count($caracter); $e > 0; $e--) {
-                      if ($caracter[$e] == ' ') {
-                          $i -= $length - $e;
-                          array_pop($caracter);
-                          break;
-                        }    
-                        array_pop($caracter);     
+        // Verifica se a linha tem espaços entre as palavras.
+        if (in_array(' ', $caracter)) {
+            // Verifica se o próximo caracter é ' '.
+            if (isset($text[$i + 1]) && $text[$i + 1] != ' ') {
+                // Elimina ' ' da linha evitando corte de palavras.
+                for ($e = count($caracter); $e > 0; $e--) {
+                    if ($caracter[$e] == ' ') {
+                        $i -= $length - $e;
+                        array_pop($caracter);
+                        break;
                     }
-                }              
+                    array_pop($caracter);
+                }
             }
-            if ($text[$i + 1] == ' ') {
-                $i += 1;
-            }        
-            // gera linha
-            $resultado[$posicao] = implode($caracter);
-            // prepara variáveis para iniciar uma nova linha
-            $posicaoCaracter = 1;
-            $caracter = [];
-            $posicao++;
-        // construção da última linha
+        }
+        if ($text[$i + 1] == ' ') {
+            $i += 1;
+        }
+        // Gera linha.
+        $resultado[$posicao] = implode($caracter);
+        // Prepara variáveis para iniciar uma nova linha.
+        $posicaoCaracter = 1;
+        $caracter = [];
+        $posicao++;
+        // Construção da última linha.
         }elseif ($i == (strlen($text)) - 1) {
             $resultado[$posicao] = implode($caracter);                 
         }
     }
     return $resultado;
-    }
+  }
 }
 ?>
