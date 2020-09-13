8b88625a9a406763361f138febb487146d72eaa6
Olympus-Sky <giovane.eevee@gmail.com>
2020-09-13 05:01:35 -0300

    Teste 10

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -116,7 +116,8 @@ class Resolucao implements Resolucao {
    * linha, separa o texto em palavras divididas por espaços com as seguintes regras:
    * - Retorne o todo o texto, com o máximo de palavras por linha, mas sem nunca extrapolar o limite de caracteres.
    * - Se uma palavra não couber na linha e o comprimento dela for menor que o limite de caracteres, ela não deve ser cortada, e sim jogada para a próxima linha.
-   * - Se a palavra for maior que o limite de caracteres por linha, corte a palavra e continue a imprimi-la na linha seguinte.
+   * - Se a palavra for maior que o limite de caracteres por linha. 
+   * Corte a palavra e continue a imprimi-la na linha seguinte.
    */
   public function textWrap(string $text, int $length): array {
     $values = new Values();
