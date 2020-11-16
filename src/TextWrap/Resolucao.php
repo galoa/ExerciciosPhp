d724f2296c5a41ceb54718d3fd7b825e2da3b81e
vitorhugoclz <37125102+vitorhugoclz@users.noreply.github.com>
2020-11-16 18:22:19 -0300

    Remoção de comentário
    Removido comentário anterior a classe de Resolução

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -2,18 +2,6 @@
 
 namespace Galoa\ExerciciosPhp\TextWrap;
 
-/**
- * Implemente sua resolução nessa classe.
- *
- * Depois disso:
- * - Crie um PR no github com seu código
- * - Veja o resultado da correção automática do seu código
- * - Commit até os testes passarem
- * - Passou tudo, melhore a cobertura dos testes
- * - Ficou satisfeito, envie seu exercício para a gente! <3
- *
- * Boa sorte :D
- */
 class Resolucao implements TextWrapInterface {
 
   /**
