c2db1151964ad18541b915813f16a3fec02ee102
vitorhugoclz <37125102+vitorhugoclz@users.noreply.github.com>
2020-11-16 17:38:25 -0300

    Exercício de PHP para back-end
    Nova resolução do exercício de php com divisão feita por "preg_split" e uso nova escrita para array

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -19,49 +19,52 @@ class Resolucao implements TextWrapInterface {
   /**
    * {@inheritdoc}
    *
-   * Apague o conteúdo do método abaixo e escreva sua própria implementação,
-   * nós colocamos esse mock para poder rodar a análise de cobertura dos
-   * testes unitários.
+   * Quebra uma string em diversas strings com tamanho passado por parâmetro.
+   *
+   * @param string $text
+   *   O texto que será utilizado como entrada.
+   * @param int $length
+   *   Em quantos caracteres a linha deverá ser quebrada.
+   *
+   * @return array
+   *   Um array de strings equivalente ao texto recebido
    */
   public function textWrap(string $text, int $length): array {
-    if ($length === 8) {
-      return [
-        'Se vi',
-        'mais',
-        'longe',
-        'foi por',
-        'estar de',
-        'pé sobre',
-        'ombros',
-        'de',
-        'gigantes',
-      ];
+    $espaco = ' ';
+    $palavrasTexto = preg_split("[ ]", $text);
+    $listaSaida = [""];
+    $linha = 0;
+    for($i = 0;$i < sizeof($palavrasTexto); $i++) {
+      $palavra = $palavrasTexto[$i];
+      if(mb_strlen($palavra) <= $length) {
+        if(mb_strlen($listaSaida[$linha]) == 0) {
+          $listaSaida[$linha] .= $palavra;
+        } else {
+          if(mb_strlen($listaSaida[$linha]) + mb_strlen($palavra) < $length) {
+            $listaSaida[$linha] .= $espaco . $palavra;
+          }
+          else {
+            $listaSaida[] = $palavra;
+            $linha++;
+          }
+        }
+      } else {
+        if(mb_strlen($listaSaida[$linha]) < $length){
+          $tamCorte = $length - mb_strlen($listaSaida[$linha]) - 1;
+          $listaSaida[$linha] .= $espaco . substr($palavra, 0, $tamCorte);
+          $palavra = substr($palavra, $tamCorte, mb_strlen($palavra));
+        }
+        while (mb_strlen($palavra) >= $length) {
+          $listaSaida[] = substr($palavra, 0, $length);
+          $palavra = substr($palavra, $length, mb_strlen($palavra));
+          $linha++;
+        }
+        if(mb_strlen($palavra) > 0) {
+          $listaSaida[] = $palavra;
+          $linha++;
+        }
+      }
     }
-    elseif ($length === 12) {
-      return [
-        'Se vi mais',
-        'longe foi',
-        'por estar de',
-        'pé sobre',
-        'ombros de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 10) {
-      // Por favor, não implemente o código desse jeito, isso é só um mock.
-      $ret = [
-        'Se vi mais',
-        'longe foi',
-        'por estar',
-        'de pé',
-        'sobre',
-      ];
-      $ret[] = 'ombros de';
-      $ret[] = 'gigantes';
-      return $ret;
-    }
-
-    return [""];
+    return $listaSaida;
   }
-
 }
