378924ba868011a3cd5b294ab71c21eafc676b24
samuel-cavalcanti <scavalcanti111@gmail.com>
2020-11-17 00:31:33 -0300

    reiniciando os testes

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -14,54 +14,57 @@ namespace Galoa\ExerciciosPhp\TextWrap;
  *
  * Boa sorte :D
  */
-class Resolucao implements TextWrapInterface {
+class Resolucao implements TextWrapInterface
+{
 
-  /**
-   * {@inheritdoc}
-   *
-   * Apague o conteúdo do método abaixo e escreva sua própria implementação,
-   * nós colocamos esse mock para poder rodar a análise de cobertura dos
-   * testes unitários.
-   */
-  public function textWrap(string $text, int $length): array {
-    if ($length === 8) {
-      return [
-        'Se vi',
-        'mais',
-        'longe',
-        'foi por',
-        'estar de',
-        'pé sobre',
-        'ombros',
-        'de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 12) {
-      return [
-        'Se vi mais',
-        'longe foi',
-        'por estar de',
-        'pé sobre',
-        'ombros de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 10) {
-      // Por favor, não implemente o código desse jeito, isso é só um mock.
-      $ret = [
-        'Se vi mais',
-        'longe foi',
-        'por estar',
-        'de pé',
-        'sobre',
-      ];
-      $ret[] = 'ombros de';
-      $ret[] = 'gigantes';
-      return $ret;
-    }
 
-    return [""];
-  }
+    /**
+     * {@inheritdoc}
+     *
+     * Apague o conteúdo do método abaixo e escreva sua própria implementação,
+     * nós colocamos esse mock para poder rodar a análise de cobertura dos
+     * testes unitários.
+     */
+    public function textWrap(string $text, int $length): array
+    {
+        if ($length === 8) {
+            return [
+                'Se vi',
+                'mais',
+                'longe',
+                'foi por',
+                'estar de',
+                'pé sobre',
+                'ombros',
+                'de',
+                'gigantes',
+            ];
+        } else if ($length === 12) {
+            return [
+                'Se vi mais',
+                'longe foi',
+                'por estar de',
+                'pé sobre',
+                'ombros de',
+                'gigantes',
+            ];
+        } else if ($length === 10) {
+            // Por favor, não implemente o código desse jeito, isso é só um mock.
+            $ret   = [
+                'Se vi mais',
+                'longe foi',
+                'por estar',
+                'de pé',
+                'sobre',
+            ];
+            $ret[] = 'ombros de';
+            $ret[] = 'gigantes';
+            return $ret;
+        }//end if
 
-}
+        return [''];
+
+    }//end textWrap()
+
+
+}//end class
--- tests/src/Unit/TextWrapTest.php
+++ tests/src/Unit/TextWrapTest.php
@@ -31,39 +31,6 @@ class TextWrapTest extends TestCase {
     $this->assertCount(1, $ret);
   }
 
-  /**
-   * Testa a quebra de linha para palavras curtas.
-   *
-   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-   */
-  public function testForSmallWords() {
-    $ret = $this->resolucao->textWrap($this->baseString, 8);
-    $this->assertEquals("Se vi", $ret[0]);
-    $this->assertEquals("mais", $ret[1]);
-    $this->assertEquals("longe", $ret[2]);
-    $this->assertEquals("foi por", $ret[3]);
-    $this->assertEquals("estar de", $ret[4]);
-    $this->assertEquals("pé sobre", $ret[5]);
-    $this->assertEquals("ombros", $ret[6]);
-    $this->assertEquals("de", $ret[7]);
-    $this->assertEquals("gigantes", $ret[8]);
-    $this->assertCount(9, $ret);
-  }
 
-  /**
-   * Testa a quebra de linha para palavras curtas.
-   *
-   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-   */
-  public function testForSmallWords2() {
-    $ret = $this->resolucao->textWrap($this->baseString, 12);
-    $this->assertEquals("Se vi mais", $ret[0]);
-    $this->assertEquals("longe foi", $ret[1]);
-    $this->assertEquals("por estar de", $ret[2]);
-    $this->assertEquals("pé sobre", $ret[3]);
-    $this->assertEquals("ombros de", $ret[4]);
-    $this->assertEquals("gigantes", $ret[5]);
-    $this->assertCount(6, $ret);
-  }
 
 }
