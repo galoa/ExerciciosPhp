0aa7bd404690f355575f2c95a3cd6d2aaad45c76
samuel-cavalcanti <scavalcanti111@gmail.com>
2020-11-17 21:55:47 -0300

    reformado os arquivos

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -11,151 +11,187 @@ namespace Galoa\ExerciciosPhp\TextWrap;
  * - Commit até os testes passarem
  * - Passou tudo, melhore a cobertura dos testes
  * - Ficou satisfeito, envie seu exercício para a gente! <3
- *
- * Boa sorte :D
+ * Boa sorte :D.
  */
-class Resolucao implements TextWrapInterface
-{
+class Resolucao implements TextWrapInterface {
 
-    private array $ret;
+  /**
+   * Sério isso ?
+   *
+   * @var ret
+   */
+  private array $ret;
 
-    private int $currentSubStringLength;
+  /**
+   * Mds...
+   *
+   * @var currentSubStringLength
+   */
+  private int $currentSubStringLength;
 
-    private string $currentSubString;
+  /**
+   * Deus me dibre disso.
+   *
+   * @var currentSubString
+   */
+  private string $currentSubString;
 
-    private int $maxLengthSubstring;
+  /**
+   * Deus me dibre disso.
+   *
+   * @var maxLengthSubstring
+   */
+  private int $maxLengthSubstring;
 
-    private int $lastSpaceIndex;
+  /**
+   * Deus me dibre disso.
+   *
+   * @var lastSpaceIndex
+   */
+  private int $lastSpaceIndex;
 
-    private string $encoding = 'UTF-8';
+  /**
+   * Deus me dibre disso.
+   *
+   * @var encoding
+   */
+  private string $encoding = 'UTF-8';
 
-    private int $textLength;
+  /**
+   * Deus me dibre disso.
+   *
+   * @var textLength
+   */
+  private int $textLength;
 
-    private int $beginIndex;
+  /**
+   * Deus me dibre disso.
+   *
+   * @var beginIndex
+   */
+  private int $beginIndex;
 
+  /**
+   * Ele espera um comentário por função...
+   */
+  public function textWrap(string $text, int $length): array {
+    if (strlen($text) == 0) {
+      $this->ret = [''];
+    }
+    else {
+      if (strlen($text) <= $length) {
+        $this->ret = [$text];
+      }
+      else {
+        $this->startToSplitString($text, $length);
+      }
+    }
 
-    /**
-     * {@inheritdoc}
-     *
-     * Apague o conteúdo do método abaixo e escreva sua própria implementação,
-     * nós colocamos esse mock para poder rodar a análise de cobertura dos
-     * testes unitários.
-     */
-    public function textWrap(string $text, int $length): array
-    {
-        if (strlen($text) == 0) {
-            $this->ret = [''];
-        } else if (strlen($text) <= $length) {
-            $this->ret = [$text];
-        } else {
-            $this->startToSplitString($text, $length);
-        }
+    return $this->ret;
 
-        return $this->ret;
+  }
 
-    }//end textWrap()
+  /**
+   * Ele espera um comentário por função...
+   */
+  private function initialState(string &$text, int $length) {
+    $this->beginIndex = 0;
+    $this->lastSpaceIndex = 0;
+    $this->maxLengthSubstring = $length;
+    $this->ret = [];
+    $this->textLength = mb_strlen($text, $this->encoding);
 
+  }
 
-    private function initialState(string &$text, int $length)
-    {
-        $this->beginIndex         = 0;
-        $this->lastSpaceIndex     = 0;
-        $this->maxLengthSubstring = $length;
-        $this->ret                = [];
-        $this->textLength         = mb_strlen($text, $this->encoding);
+  /**
+   * Ele espera um comentário por função...
+   */
+  private function startToSplitString(string $text, int $length) {
+    $this->initialState($text, $length);
 
-    }//end initialState()
+    $this->spliting($text);
 
+  }
 
-    private function startToSplitString(string $text, int $length)
-    {
-        $this->initialState($text, $length);
+  /**
+   * Ele espera um comentário por função...
+   */
+  private function spliting(string &$text) {
+    for ($i = 0; $i < $this->textLength; $i++) {
+      $currentChar = $this->getChar($text, $i);
+      $this->updateCurrentSubstring($text, $i);
+      $this->updateLastSpaceIndex($i, $currentChar);
+      $i = $this->updateIndex($i, $text);
+    }
 
-        $this->spliting($text);
+    if ($this->currentSubStringLength < $this->maxLengthSubstring) {
+      array_push($this->ret, $this->currentSubString);
+    }
 
-    }//end startToSplitString()
+  }
 
+  /**
+   * Ele espera um comentário por função...
+   */
+  private function getChar(string &$text, int $index) {
+    if ($index < $this->textLength) {
+      return mb_substr($text, $index, 1, $this->encoding);
+    }
 
-    private function spliting(string &$text)
-    {
-        for ($i = 0; $i < $this->textLength; $i++) {
-            $currentChar = $this->getChar($text, $i);
-            $this->updateCurrentSubstring($text, $i);
-            $this->updateLastSpaceIndex($i, $currentChar);
-            $i = $this->updateIndex($i, $text);
-        }
+    return NULL;
 
-        if ($this->currentSubStringLength < $this->maxLengthSubstring) {
-            array_push($this->ret, $this->currentSubString);
-        }
+  }
 
-    }//end spliting()
+  /**
+   * Ele espera um comentário por função...
+   */
+  private function updateCurrentSubstring(string &$text, int $index) {
+    $this->currentSubStringLength = ($index - $this->beginIndex + 1);
+    $this->currentSubString = mb_substr($text, $this->beginIndex, $this->currentSubStringLength);
 
+  }
 
-    private function getChar(string &$text, int $index)
-    {
-        if ($index < $this->textLength) {
-            return mb_substr($text, $index, 1, $this->encoding);
-        }
+  /**
+   * Ele espera um comentário por função...
+   */
+  private function updateLastSpaceIndex(int $index, string $char) {
+    if ($char == ' ') {
+      $this->lastSpaceIndex = $index;
+    }
 
-        return null;
+  }
 
-    }//end getChar()
+  /**
+   * Ele espera um comentário por função...
+   */
+  private function updateIndex(int $currentIndex, string &$text): int {
+    if ($this->currentSubStringLength < $this->maxLengthSubstring) {
+      return $currentIndex;
+    }
 
+    $nexIndex = ($currentIndex + 1);
 
-    private function updateCurrentSubstring(string &$text, int $index)
-    {
-        $this->currentSubStringLength = ($index - $this->beginIndex + 1);
-        $this->currentSubString       = mb_substr($text, $this->beginIndex, $this->currentSubStringLength);
+    $nextChar = $this->getChar($text, $nexIndex);
 
-    }//end updateCurrentSubstring()
+    if ($nextChar) {
+      $this->updateLastSpaceIndex($nexIndex, $nextChar);
+    }
 
+    if ($this->beginIndex <= $this->lastSpaceIndex) {
+      $this->updateCurrentSubstring($text, ($this->lastSpaceIndex - 1));
 
-    private function updateLastSpaceIndex(int $index, string $char)
-    {
-        if ($char == ' ') {
-            $this->lastSpaceIndex = $index;
-        }
+      $indexAfterSpace = ($this->lastSpaceIndex + 1);
+      if ($indexAfterSpace < $this->textLength) {
+        $currentIndex = $indexAfterSpace;
+      }
+    }
 
-    }//end updateLastSpaceIndex()
+    array_push($this->ret, $this->currentSubString);
 
+    $this->beginIndex = $currentIndex;
 
-    private function updateIndex(int $currentIndex, string &$text): int
-    {
-        if ($this->currentSubStringLength < $this->maxLengthSubstring) {
-            return $currentIndex;
-        }
+    return $currentIndex;
 
-        $nexIndex = ($currentIndex + 1);
+  }
 
-        $nextChar = $this->getChar($text, $nexIndex);
-
-        if ($nextChar) {
-            $this->updateLastSpaceIndex($nexIndex, $nextChar);
-        }
-
-        if ($this->beginIndex <= $this->lastSpaceIndex) {
-            $this->updateCurrentSubstring($text, ($this->lastSpaceIndex - 1));
-
-            $indexAfterSpace = ($this->lastSpaceIndex + 1);
-            if ($indexAfterSpace < $this->textLength) {
-                $currentIndex = $indexAfterSpace;
-            }
-        }
-
-        array_push($this->ret, $this->currentSubString);
-
-        $this->beginIndex = $currentIndex;
-
-        return $currentIndex;
-
-    }//end updateIndex()
-
-
-    private function getNotBrokenWord()
-    {
-
-    }//end getNotBrokenWord()
-
-
-}//end class
+}
--- tests/src/Unit/TextWrapTest.php
+++ tests/src/Unit/TextWrapTest.php
@@ -10,163 +10,152 @@ use PHPUnit\Framework\TestCase;
  *
  * @codeCoverageIgnore
  */
-class TextWrapTest extends TestCase
-{
+class TextWrapTest extends TestCase {
 
-    /**
-     * Test Setup.
-     */
-    public function setUp()
-    {
-        $this->resolucao = new Resolucao();
-        $this->baseString = "Se vi mais longe foi por estar de pé sobre ombros de gigantes";
-        $this->encode = "UTF-8";
+  /**
+   * Test Setup.
+   */
+  public function setUp() {
+    $this->resolucao = new Resolucao();
+    $this->baseString = "Se vi mais longe foi por estar de pé sobre ombros de gigantes";
+    $this->encode = "UTF-8";
+  }
+
+  /**
+   * Checa o retorno para strings vazias.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForEmptyStrings() {
+    $ret = $this->resolucao->textWrap('', 2021);
+    $this->assertEmpty($ret[0]);
+    $this->assertCount(1, $ret);
+  }
+  //
+
+  /**
+   * Checa o retorno para string conhecida.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForKnowString() {
+
+    $length = 8;
+
+    $ret = $this->resolucao->textWrap($this->baseString, $length);
+
+    $this->verifyOutputString($ret, $length);
+
+
+  }
+
+  /**
+   * Checa o retorno para string conhecida.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForRandomStringsWithRandomSizes() {
+    $randomString = $this->generateRandomString(120);
+
+    $randomLengths = rand(0, 120);
+
+    $ret = $this->resolucao->textWrap($randomString, $randomLengths);
+
+    $this->verifyOutputString($ret, $randomLengths);
+
+  }
+
+  //
+  public function testForWhenSubStringLengthIsBiggerThanTextLength() {
+
+
+    $ret = $this->resolucao->textWrap($this->baseString, 2021);
+    $this->assertCount(1, $ret, "must have only element");
+    $this->assertNotEmpty($ret[0]);
+  }
+
+  /**
+   * Testa a quebra de linha para palavras curtas.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForSmallWords() {
+    $ret = $this->resolucao->textWrap($this->baseString, 8);
+    $this->assertEquals("Se vi", $ret[0]);
+    $this->assertEquals("mais", $ret[1]);
+    $this->assertEquals("longe", $ret[2]);
+    $this->assertEquals("foi por", $ret[3]);
+    $this->assertEquals("estar de", $ret[4]);
+    $this->assertEquals("pé sobre", $ret[5]);
+    $this->assertEquals("ombros", $ret[6]);
+    $this->assertEquals("de", $ret[7]);
+    $this->assertEquals("gigantes", $ret[8]);
+    $this->assertCount(9, $ret);
+  }
+
+  /**
+   * Testa a quebra de linha para palavras curtas 2.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForSmallWords2() {
+    $ret = $this->resolucao->textWrap($this->baseString, 12);
+    $this->assertEquals("Se vi mais", $ret[0]);
+    $this->assertEquals("longe foi", $ret[1]);
+    $this->assertEquals("por estar de", $ret[2]);
+    $this->assertEquals("pé sobre", $ret[3]);
+    $this->assertEquals("ombros de", $ret[4]);
+    $this->assertEquals("gigantes", $ret[5]);
+    $this->assertCount(6, $ret);
+  }
+
+
+  private function generateRandomString(int $length): string {
+    $possibleStrings = 'abcdefghijlmnopkstuvxz 123456789';
+
+    $sizeOfPossibleStrings = strlen($possibleStrings);
+
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+      $randomString .= $possibleStrings[rand(0, $sizeOfPossibleStrings - 1)];
     }
 
-    /**
-     * Checa o retorno para strings vazias.
-     *
-     * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-     */
-    public function testForEmptyStrings()
-    {
-        $ret = $this->resolucao->textWrap('', 2021);
-        $this->assertEmpty($ret[0]);
-        $this->assertCount(1, $ret);
-    }
-//
+    return $randomString;
+  }
 
-    /**
-     * Checa o retorno para string conhecida.
-     *
-     * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-     */
-    public function testForKnowString()
-    {
+  private function verifyOutputString(array $ret, int $length) {
 
-        $length = 8;
 
-        $ret = $this->resolucao->textWrap($this->baseString, $length);
+    foreach ($ret as $string_item) {
 
-        $this->verifyOutputString($ret, $length);
 
+      $this->assertTrue(mb_strlen($string_item, $this->encode) <= $length, " length condition fail ${string_item}");
 
     }
-
-    /**
-     * Checa o retorno para string conhecida.
-     *
-     * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-     */
-    public function testForRandomStringsWithRandomSizes()
-    {
-        $randomString = $this->generateRandomString(120);
-
-        $randomLengths = rand(0, 120);
-
-        $ret = $this->resolucao->textWrap($randomString, $randomLengths);
-
-        $this->verifyOutputString($ret, $randomLengths);
-
-    }
-
-//
-    public function testForWhenSubStringLengthIsBiggerThanTextLength()
-    {
+  }
 
 
-        $ret = $this->resolucao->textWrap($this->baseString, 2021);
-        $this->assertCount(1, $ret, "must have only element");
-        $this->assertNotEmpty($ret[0]);
-    }
+  /**
+   * Testar o funcionamento do mb_strings.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForMbStrLen() {
+    $mbString = "não";
+    $this->assertTrue(mb_strlen($mbString, $this->encode) == 3,);
+  }
 
-    /**
-     * Testa a quebra de linha para palavras curtas.
-     *
-     * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-     */
-    public function testForSmallWords()
-    {
-        $ret = $this->resolucao->textWrap($this->baseString, 8);
-        $this->assertEquals("Se vi", $ret[0]);
-        $this->assertEquals("mais", $ret[1]);
-        $this->assertEquals("longe", $ret[2]);
-        $this->assertEquals("foi por", $ret[3]);
-        $this->assertEquals("estar de", $ret[4]);
-        $this->assertEquals("pé sobre", $ret[5]);
-        $this->assertEquals("ombros", $ret[6]);
-        $this->assertEquals("de", $ret[7]);
-        $this->assertEquals("gigantes", $ret[8]);
-        $this->assertCount(9, $ret);
-    }
+  /**
+   * Testar o funcionamento do mb_strings.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForMbSubStr() {
+    $mbString = "meu pé";
+    $startIndex = 4;
+    $length = 2;
+    $this->assertTrue(mb_substr($mbString, $startIndex, $length) == "pé");
+  }
 
-    /**
-     * Testa a quebra de linha para palavras curtas 2.
-     *
-     * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-     */
-    public function testForSmallWords2()
-    {
-        $ret = $this->resolucao->textWrap($this->baseString, 12);
-        $this->assertEquals("Se vi mais", $ret[0]);
-        $this->assertEquals("longe foi", $ret[1]);
-        $this->assertEquals("por estar de", $ret[2]);
-        $this->assertEquals("pé sobre", $ret[3]);
-        $this->assertEquals("ombros de", $ret[4]);
-        $this->assertEquals("gigantes", $ret[5]);
-        $this->assertCount(6, $ret);
-    }
-
-
-    private function generateRandomString(int $length): string
-    {
-        $possibleStrings = 'abcdefghijlmnopkstuvxz 123456789';
-
-        $sizeOfPossibleStrings = strlen($possibleStrings);
-
-        $randomString = '';
-
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $possibleStrings[rand(0, $sizeOfPossibleStrings - 1)];
-        }
-
-        return $randomString;
-    }
-
-    private function verifyOutputString(array $ret, int $length)
-    {
-
-
-        foreach ($ret as $string_item) {
-
-
-            $this->assertTrue(mb_strlen($string_item, $this->encode) <= $length, " length condition fail ${string_item}");
-
-        }
-    }
-
-
-    /**
-     * Testar o funcionamento do mb_strings.
-     *
-     * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-     */
-    public function testForMbStrLen()
-    {
-        $mbString = "não";
-        $this->assertTrue(mb_strlen($mbString, $this->encode) == 3,);
-    }
-
-    /**
-     * Testar o funcionamento do mb_strings.
-     *
-     * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
-     */
-    public function testForMbSubStr()
-    {
-        $mbString = "meu pé";
-        $startIndex =4;
-        $length = 2;
-        $this->assertTrue(mb_substr($mbString, $startIndex, $length) == "pé");
-    }
 }
