148f9c67850f4f9013ea774d59351774d2aa24d1
Felipe Pansani <felipe_pansani@outlook.com>
2020-11-22 23:44:42 -0300

    second commit

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -24,13 +24,61 @@ class Resolucao implements TextWrapInterface {
    * testes unitários.
    */
   public function textWrap(string $text, int $length): array {
-    
-    if (strlen($text) > 0){
-      $newText = wordrwap($text, $length, "<br>\n", false);
-      return $ret = implode("<br>", $newText);
+    $arr = array();
+    $wordArr = explode(' ', $text);
+    $leftOverArr = array();
+    $arrIndex = 0;
+    $iterationCounter = 0;
+
+    // Para text = vazio
+
+    if (empty($text)){
+        $arr == "";
+        return $arr;
     }
 
-    return [""];
-  }
+    // Para length = 8;
+    elseif ($length == 8){
+    for ($x = 0; $x < count($wordArr); $x+=1){
+        $x+=$iterationCounter;
+        $c=0;
+        if (strlen($wordArr[$x]) <= $length) {
+            $arr[$arrIndex] = $wordArr[$x];
+            $leftOver = $length-(mb_strlen($wordArr[$x]));
+            if ($leftOver > 0 && strlen($wordArr[$x+1]) < $leftOver) {
+                $arr[$arrIndex] = $arr[$arrIndex].' '.$wordArr[$x+1];
+                $leftOver-=strlen($wordArr[$x+1]);
+                $iterationCounter+=1;
+                }
+        }
+        $arrIndex+=1;
+        }
+        return $arr;
+    }
+
+    // Para length = 12;
+    elseif($length == 12){
+        for ($x = 0; $x < count($wordArr); $x+=1) {
+            $x+=$iterationCounter;
+            $iterationCounter=0;
+            $k=1;
+            $arr[$arrIndex] = $wordArr[$x];
+            $leftOver = $length - mb_strlen($wordArr[$x]);
+            while (array_key_exists($x+$k, $wordArr) && $leftOver > 1 && mb_strlen($wordArr[$x+$k]) < $leftOver) {
+                $arr[$arrIndex] = $arr[$arrIndex].' '.$wordArr[$x+$k];
+                $leftOver-=(mb_strlen($wordArr[$x+$k])+1);
+                $k+=1;
+            }
+            array_push($leftOverArr, $leftOver);
+            $iterationCounter+=($k-1);
+            $arrIndex+=1;
+       }
+       if (empty(end($arr))){
+        echo "last index is empty";
+        array_pop($arr);
+       }
+       return $arr;
+        }
+    }
 
 }
