86e742a67447ca3732f89b89db562b627444320c
Felipe Pansani <felipe_pansani@outlook.com>
2020-11-29 02:33:53 -0300

    16th commit

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -32,42 +32,43 @@ class Resolucao implements TextWrapInterface {
     if (empty($text)) {
       $arr[0] = "";
       return $arr;
-    } 
+    }
     else {
       for ($x = 0; $x + $counter < count($wordArr); $x += 1) {
-          $x += $counter;
-          $counter = 0;
-          $iterationCounter = 1;
-          if (mb_strlen($wordArr[$x]) <= $length) {
-            $arr[$index] = $wordArr[$x];
-            $leftOver = $length - mb_strlen($wordArr[$x]);
-              while (
+        $x += $counter;
+        $counter = 0;
+        $iterationCounter = 1;
+        if (mb_strlen($wordArr[$x]) <= $length) {
+          $arr[$index] = $wordArr[$x];
+          $leftOver = $length - mb_strlen($wordArr[$x]);
+            while (
                   array_key_exists($x + $iterationCounter, $wordArr)
                   && mb_strlen($wordArr[$x + $iterationCounter]) < $leftOver && $leftOver > 1
                 ) {
-                    $arr[$index] = $arr[$index] . ' ' . $wordArr[$x + $iterationCounter];
-                    $leftOver -= (mb_strlen($wordArr[$x + $iterationCounter]) + 1);
-                    $iterationCounter += 1;
-                }
-                $counter += ($iterationCounter - 1);
-                $index += 1;
-            } else {
-                $newArr = mb_str_split($wordArr[$x], $length);
-                for ($z = 0; $z < count($newArr); $z += 1) {
-                    $arr[$index + $z] = $newArr[$z];
-                    $leftOver = $length - mb_strlen($newArr[$z]);
-                    while (array_key_exists($x + $iterationCounter, $wordArr)
+            $arr[$index] = $arr[$index] . ' ' . $wordArr[$x + $iterationCounter];
+            $leftOver -= (mb_strlen($wordArr[$x + $iterationCounter]) + 1);
+            $iterationCounter += 1;
+          }
+          $counter += ($iterationCounter - 1);
+          $index += 1;
+        } 
+        else {
+          $newArr = mb_str_split($wordArr[$x], $length);
+            for ($z = 0; $z < count($newArr); $z += 1) {
+              $arr[$index + $z] = $newArr[$z];
+              $leftOver = $length - mb_strlen($newArr[$z]);
+              while (array_key_exists($x + $iterationCounter, $wordArr)
                     && mb_strlen($wordArr[$x + $iterationCounter]) < $leftOver && $leftOver > 1) {
-                        $arr[$index+$z] = $arr[$index+$z] . ' ' . $wordArr[$x+$iterationCounter];
-                        $leftOver -= (mb_strlen($wordArr[$x + $iterationCounter]) + 1);
-                        $iterationCounter+=1;
-                    }
-                }
-                $counter += ($iterationCounter - 1);
-                $index += $z;
-            }
-        }
-    }
+                  $arr[$index + $z] = $arr[$index + $z] . ' ' . $wordArr[$x + $iterationCounter];
+                  $leftOver -= (mb_strlen($wordArr[$x + $iterationCounter]) + 1);
+                  $iterationCounter+=1;
+                  }
+              }
+              $counter += ($iterationCounter - 1);
+              $index += $z;
+          }
+      }
+  }
     if (empty(end($arr))) {
         array_pop($arr);
     }
