19bc79a0bf7a124d32e8e0bf4650afbd0dd3f312
Lucas Ribeiro <lucas.mr@aluno.ufop.edu.br>
2020-11-29 10:58:08 -0300

    ✨  textWrap implementado

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -2,66 +2,39 @@
 
 namespace Galoa\ExerciciosPhp\TextWrap;
 
-/**
- * Implemente sua resolução nessa classe.
- *
- * Depois disso:
- * - Crie um PR no github com seu código
- * - Veja o resultado da correção automática do seu código
- * - Commit até os testes passarem
- * - Passou tudo, melhore a cobertura dos testes
- * - Ficou satisfeito, envie seu exercício para a gente! <3
- *
- * Boa sorte :D
- */
+
 class Resolucao implements TextWrapInterface {
 
-  /**
-   * {@inheritdoc}
-   *
-   * Apague o conteúdo do método abaixo e escreva sua própria implementação,
-   * nós colocamos esse mock para poder rodar a análise de cobertura dos
-   * testes unitários.
-   */
-  public function textWrap(string $text, int $length): array {
-    if ($length === 8) {
-      return [
-        'Se vi',
-        'mais',
-        'longe',
-        'foi por',
-        'estar de',
-        'pé sobre',
-        'ombros',
-        'de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 12) {
-      return [
-        'Se vi mais',
-        'longe foi',
-        'por estar de',
-        'pé sobre',
-        'ombros de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 10) {
-      // Por favor, não implemente o código desse jeito, isso é só um mock.
-      $ret = [
-        'Se vi mais',
-        'longe foi',
-        'por estar',
-        'de pé',
-        'sobre',
-      ];
-      $ret[] = 'ombros de';
-      $ret[] = 'gigantes';
-      return $ret;
-    }
+  public function textWrap(string $text, int $length): array
+  {
+      $arrText = explode(" ", $text);
+      $lines = [];
+      $line = "";
+      foreach ($arrText as $word) {
+          if (strlen($word) <= $length){
+              if(strlen($line) > strlen($word)) { // Usando > ao inves de >= para caber o espaço
+                  $line = $line ." ". $word;
+              }else{
+                  $lines.append($line);
+                  $line = "";
+                  $line = $line ." ". $word; // A palavra nunca será maior que a linha, por isso não é necessario verificação   
+              }
+          }else{
+              $counter = 0;
+              for ($i=0; $i < strlen($word); $i++) { 
+                  if($counter < $length){ // < para dar espaço ao ultimo caractere no else, se não perderia a letra
+                      $line += $word[$i];
+                      $counter++;
+                  }else{
+                      $counter = 0;
+                      $line += $word[$i];
+                      $lines.append($line);
+                      $line = "";
+                  }
+              }
+          }
 
-    return [""];
+      }
+      return $lines;
   }
-
 }
