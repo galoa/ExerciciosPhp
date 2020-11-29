b9693a6c84dc6a904d4ad8cb72d22c47c8448bb2
Lucas Ribeiro <lucas.mr@aluno.ufop.edu.br>
2020-11-29 11:11:54 -0300

    🐛  Fix append

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -3,38 +3,38 @@
 namespace Galoa\ExerciciosPhp\TextWrap;
 
 
-class Resolucao implements TextWrapInterface {
+class Resolucao implements TextWrapInterface
+{
 
   public function textWrap(string $text, int $length): array
   {
-      $arrText = explode(" ", $text);
-      $lines = [];
-      $line = "";
-      foreach ($arrText as $word) {
-          if (strlen($word) <= $length){
-              if(strlen($line) > strlen($word)) { // Usando > ao inves de >= para caber o espaço
-                  $line = $line ." ". $word;
-              }else{
-                  $lines.append($line);
-                  $line = "";
-                  $line = $line ." ". $word; // A palavra nunca será maior que a linha, por isso não é necessario verificação   
-              }
-          }else{
-              $counter = 0;
-              for ($i=0; $i < strlen($word); $i++) { 
-                  if($counter < $length){ // < para dar espaço ao ultimo caractere no else, se não perderia a letra
-                      $line += $word[$i];
-                      $counter++;
-                  }else{
-                      $counter = 0;
-                      $line += $word[$i];
-                      $lines.append($line);
-                      $line = "";
-                  }
-              }
+    $arrText = explode(" ", $text);
+    $lines = array();
+    $line = "";
+    foreach ($arrText as $word) {
+      if (strlen($word) <= $length) {
+        if (strlen($line) > strlen($word)) { // Usando > ao inves de >= para caber o espaço
+          $line = $line . " " . $word;
+        } else {
+          $lines[] = $line;
+          $line = "";
+          $line = $line . " " . $word; // A palavra nunca será maior que a linha, por isso não é necessario verificação   
+        }
+      } else {
+        $counter = 0;
+        for ($i = 0; $i < strlen($word); $i++) {
+          if ($counter < $length) { // < para dar espaço ao ultimo caractere no else, se não perderia a letra
+            $line += $word[$i];
+            $counter++;
+          } else {
+            $counter = 0;
+            $line += $word[$i];
+            $lines[] = $line;
+            $line = "";
           }
-
+        }
       }
-      return $lines;
+    }
+    return $lines;
   }
 }
