b57d9c66021c5114bb953c61d88fe25fa86c5fef
Lucas Ribeiro <lucas.mr@aluno.ufop.edu.br>
2020-11-30 12:36:41 -0300

    🐛  Corrigido problema de tamanho de strings

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Galoa\ExerciciosPhp\TextWrap;
 
 
@@ -8,33 +7,39 @@ class Resolucao implements TextWrapInterface
 
   public function textWrap(string $text, int $length): array
   {
+      if(mb_strlen($text, 'UTF-8') == 0)
+          return array(null);
     $arrText = explode(" ", $text);
     $lines = array();
     $line = "";
     foreach ($arrText as $word) {
-      if (strlen($word) <= $length) {
-        if (strlen($line) > strlen($word)) { // Usando > ao inves de >= para caber o espaço
-          $line = $line . " " . $word;
+        if (mb_strlen($word, 'UTF-8') <= $length) {
+            if ($length - mb_strlen($line, 'UTF-8') > mb_strlen($word, 'UTF-8') || $length - mb_strlen($line, 'UTF-8') == mb_strlen($word, 'UTF-8')+1) { // Usando > para caber o espaco
+                $line = mb_strlen($line, 'UTF-8') == 0 ? $line . $word : $line . " " . $word;
+            } else {
+                $lines[] = $line;
+//          Limpa ao mesmo tempo que adiciona a palavra
+                $line = $word; // A palavra nunca será maior que a linha, por isso não é necessario verificação
+            }
         } else {
-          $lines[] = $line;
-          $line = "";
-          $line = $line . " " . $word; // A palavra nunca será maior que a linha, por isso não é necessario verificação   
-        }
-      } else {
-        $counter = 0;
-        for ($i = 0; $i < strlen($word); $i++) {
-          if ($counter < $length) { // < para dar espaço ao ultimo caractere no else, se não perderia a letra
-            $line += $word[$i];
-            $counter++;
-          } else {
             $counter = 0;
-            $line += $word[$i];
-            $lines[] = $line;
-            $line = "";
-          }
+            for ($i = 0; $i < mb_strlen($word, 'UTF-8'); $i++) {
+                if ($counter < $length) { // < para dar espaço ao ultimo caractere no else, se não perderia a letra
+                    $line += $word[$i];
+                    $counter++;
+                } else {
+                    $counter = 0;
+                    $line += $word[$i];
+                    $lines[] = $line;
+                    $line = "";
+                }
+            }
         }
-      }
     }
+    if(mb_strlen($line, 'UTF-8') != 0)
+        $lines[] = $line;
     return $lines;
   }
 }
+//$res = new Resolucao();
+//$res->textWrap("Se vi mais longe foi por estar de pé sobre ombros de gigantes", 8);
