e3333697cfc3855bcbd458b26b775297cdf089a5
jashscript <jash.geral@gmail.com>
2020-12-02 23:09:59 -0300

    ainda arrumando o caso de 2 caracteres

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -65,6 +65,8 @@ class Resolucao implements TextWrapInterface {
           elseif (mb_strlen($linha) == $length) {
             array_push($resultado, $linha);
             $linha = "";
+          } else {
+            array_push($resultado, $linha);
           }
           // Se for a última string do array adiciona a linha ao resultado.
           if ($index == count($inputArray) - 1) {
