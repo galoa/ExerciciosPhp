822fd120619cccd5b4f610d299c972470110e871
jashscript <jash.geral@gmail.com>
2020-12-02 23:53:37 -0300

    primeira tentativa de abordagem diferente

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -36,146 +36,49 @@ class Resolucao implements TextWrapInterface {
     A variável linha é para ser usada como variável temporária/auxiliar.
      */
     $inputArray = explode(" ", $text);
+    $temp = "";
     $linha = "";
     $resultado = [];
     /*
     Percorre o array pegando tanto o index quanto a string em si.
     Nessa abordagem eu uso o index apenas para verificar se é a última palavra.
      */
-    foreach ($inputArray as $index => $palavra) {
-      // Um switch TRUE, porque acho mais organizado que uma cadeia de if/else.
-      switch (TRUE) {
-        /*
-        Primeira alternativa:
-        O número de caracteres da palavra é maior que o tamanho máximo.
-         */
-        case (mb_strlen($palavra) > $length):
-          /* Adiciona ao resultado uma substring com o tamanho máximo,
-          retirada da string. */
-          array_push($resultado, substr($palavra, 0, $length));
-          // E adiciona à variável temporária $linha o restante.
-          $linha = substr($palavra, $length);
-          if (mb_strlen($linha) > $length) {
-            do {
-              array_push($resultado, substr($linha, 0, $length));
-              $linha = substr($linha, $length);
-            } while (mb_strlen($linha) > $length);
-          }
-          elseif (mb_strlen($linha) == $length) {
-            array_push($resultado, $linha);
-            $linha = "";
-          }
-          // Se for a última string do array adiciona a linha ao resultado.
-          if ($index == count($inputArray) - 1) {
-            array_push($resultado, $linha);
-          }
-          break;
+    for ($i = 0; $i < $length; $i++) {
+      $temp = $inputArray[$i];
+      while (mb_strlen($temp) > $length) {
+        array_push($resultado, substr($temp, 0, $length));
+        $temp = substr($temp, $length);
+      }
 
-        /* Segunda alternativa:
-        O número de caracteres da palavra é igual ao tamanho máximo.
-         */
-        case (mb_strlen($palavra) == $length):
-          /* Adiciona diretamente nesse caso, e zera a
-          variável auxiliar */
-          if (mb_strlen($linha) < 1) {
-            array_push($resultado, $palavra);
-          }
-          else {
-            array_push($resultado, $linha);
-            array_push($resultado, $palavra);
-          }
-          break;
+      if(mb_strlen($temp) > 0){
+        $linha = $temp;
+      }
 
-        /* Terceira alterantiva:
-        O número de caracteres da variável auxiliar $linha mais a palavra
-        é maior que o tamanho máximo, já que uso a variável auxiliar pra guardar
-        o que sobrou da linha anterior.
-         */
-        case (mb_strlen($linha . $palavra) > $length):
-          // Adiciona ao resultado a linha.
+      switch(true) {
+        case (mb_strlen($linha) == $length):
           array_push($resultado, $linha);
-          // A palavra é armazenada na variável auxiliar.
-          $linha = $palavra;
-          /* Verifica se é a última linha e, se for,
-          adiciona linha ao resultado. */
-          if ($index == count($inputArray) - 1) {
-            array_push($resultado, $linha);
-          }
-          break;
+        break;
 
-        /*
-        Quarta alternativa:
-        O número de caracteres da variável auxiliar mais a palavra
-        somados com 1 (pra representar o espaço) é menor que o tamanho máximo.
-         */
-        case (mb_strlen($linha . $palavra) + 1 < $length):
-          // Se a variável auxiliar estiver vazia a palavra ocupa o lugar dela.
-          if (mb_strlen($linha) < 1) {
-            $linha = $palavra;
-          }
-          /* Se não, a variável auxiliar guarda o que havia nela concatenado
-          com um espaço e a palavra. */
-          else {
-            $linha = $linha . " " . $palavra;
-            /* Se for a última string, adiciona ao
-            resultado a variável auxiliar. */
-            if ($index == count($inputArray) - 1) {
-              array_push($resultado, $linha);
-            }
-          }
-          break;
+        case ((mb_strlen($linha . $inputArray[$i]) + 1) < $length):
+            $linha = $linha . " " . $inputArray[$i];
+        break;
 
-        /*
-        Quinta alternativa:
-        O número de caracteres da variável auxiliar mais a palavra
-        somados com 1 é igual ao tamanho máximo.
-        fiz dessa forma para poder esvaziar variável auxiliar
-        e também adicionar ao resultado diretamente.
-         */
-        case (mb_strlen($linha . $palavra) + 1 == $length):
-          if (mb_strlen($linha) < 1) {
-            $linha = $palavra;
-            /* Se for a última string, adiciona ao resultado
-            a variável auxiliar. */
-            if ($index == count($inputArray) - 1) {
-              array_push($resultado, $linha);
-            }
+        case ((mb_strlen($linha . $inputArray[$i]) + 1) == $length):
+          if(mb_strlen($linha) <1){
+            array_push($resultado,$inputArray[$i]);
+          } else {
+            array_push($resultado, $linha . " " . $inputArray[$i]);
           }
-          /*
-          Se não for, concatena na variável auxiliar um espaço e a palavra,
-          adiciona ao resultado e esvazia a variável auxiliar
-           */
-          else {
-            $linha = $linha . " " . $palavra;
-            array_push($resultado, $linha);
-            $linha = "";
-          }
-          break;
+        break;
 
-        /*
-        Sexta alternativa:
-        o número de caracteres da variável auxiliar mais um (espaço)
-        é maior que o tamanho máximo.
-         */
-        case (mb_strlen($linha . $palavra) + 1 > $length):
-          /*
-          Adiciona ao resultado a variável auxiliar,
-          carregando o resto da última linha adicionada.
-           */
+        case ((mb_strlen($linha . $inputArray[$i]) + 1) > $length):
           array_push($resultado, $linha);
-          // A variável auxiliar agora guarda a palavra atual.
-          $linha = $palavra;
-          // Se for a última string do array ela é adicionada ao resultado.
-          if ($index == count($inputArray) - 1) {
-            array_push($resultado, $linha);
-          }
-          break;
-
+          $linha = $inputArray;
+        break;
       }
     }
 
     return $resultado;
-
   }
 
 }
