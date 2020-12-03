ddebf55e52160d3e25e5fbde69fc2ee95aa4abf8
jashscript <jash.geral@gmail.com>
2020-12-02 21:12:22 -0300

    arrumando comentarios

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -25,29 +25,29 @@ class Resolucao implements TextWrapInterface {
    */
   public function textWrap(string $text, int $length): array {
     /*
-     Verifica se o input está vazio e retorna uma array
-     com um elemento null caso esteja.
+    Verifica se o input está vazio e retorna uma array
+    com um elemento null caso esteja.
      */
     if (mb_strlen($text) < 1) {
       return [NULL];
     }
-    /** 
-     * Transforma o input em uma array de strings.
-     * A variável linha é para ser usada como variável temporária/auxiliar.
+    /*
+    Transforma o input em uma array de strings.
+    A variável linha é para ser usada como variável temporária/auxiliar.
     */
     $inputArray = explode(" ", $text);
     $linha = "";
     $resultado = [];
     /* 
-     Percorre o array pegando tanto o index quanto a string em si.
-     Nessa abordagem eu uso o index apenas para verificar se é a última palavra.
+    Percorre o array pegando tanto o index quanto a string em si.
+    Nessa abordagem eu uso o index apenas para verificar se é a última palavra.
     */
     foreach ($inputArray as $index => $palavra) {
       // Um switch TRUE, porque acho mais organizado que uma cadeia de if/else.
       switch (TRUE) {
         /* 
-         Primeira alternativa:
-         O número de caracteres da palavra é maior que o tamanho máximo.
+        Primeira alternativa:
+        O número de caracteres da palavra é maior que o tamanho máximo.
         */
         case (mb_strlen($palavra) > $length):
           // Adiciona ao resultado uma substring com o tamanho máximo, retirada da string.
@@ -61,9 +61,9 @@ class Resolucao implements TextWrapInterface {
           break;
 
         /* Segunda alterantiva:
-         O número de caracteres da variável auxiliar $linha mais a palavra
-         é maior que o tamanho máximo, já que uso a variável auxiliar pra guardar
-         o que sobrou da linha anterior.
+        O número de caracteres da variável auxiliar $linha mais a palavra
+        é maior que o tamanho máximo, já que uso a variável auxiliar pra guardar
+        o que sobrou da linha anterior.
         */
         case (mb_strlen($linha . $palavra) > $length):
           // Adiciona ao resultado a linha.
@@ -77,9 +77,9 @@ class Resolucao implements TextWrapInterface {
           break;
         
         /*
-         Terceira alternativa:
-         O número de caracteres da variável auxiliar mais a palavra
-         somados com 1 (pra representar o espaço) é menor que o tamanho máximo.
+        Terceira alternativa:
+        O número de caracteres da variável auxiliar mais a palavra
+        somados com 1 (pra representar o espaço) é menor que o tamanho máximo.
         */
         case (mb_strlen($linha . $palavra) + 1 < $length):
           // Se a variável auxiliar estiver vazia a palavra ocupa o lugar dela.
@@ -97,11 +97,11 @@ class Resolucao implements TextWrapInterface {
           break;
         
         /*
-         Quarta alternativa:
-         O número de caracteres da variável auxiliar mais a palavra
-         somados com 1 é igual ao tamanho máximo.
-         fiz dessa forma para poder esvaziar variável auxiliar
-         e também adicionar ao resultado diretamente.
+        Quarta alternativa:
+        O número de caracteres da variável auxiliar mais a palavra
+        somados com 1 é igual ao tamanho máximo.
+        fiz dessa forma para poder esvaziar variável auxiliar
+        e também adicionar ao resultado diretamente.
         */
         case (mb_strlen($linha . $palavra) + 1 == $length):
           if (mb_strlen($linha) < 1) {
@@ -112,8 +112,8 @@ class Resolucao implements TextWrapInterface {
             }
           }
           /*
-           Se não for, concatena na variável auxiliar um espaço e a palavra,
-           adiciona ao resultado e esvazia a variável auxiliar
+          Se não for, concatena na variável auxiliar um espaço e a palavra,
+          adiciona ao resultado e esvazia a variável auxiliar
           */
           else {
             $linha = $linha . " " . $palavra;
@@ -122,9 +122,9 @@ class Resolucao implements TextWrapInterface {
           }
           break;
         /*
-         Quarta alternativa:
-         o número de caracteres da variável auxiliar mais um (espaço)
-         é maior que o tamanho máximo.
+        Quarta alternativa:
+        o número de caracteres da variável auxiliar mais um (espaço)
+        é maior que o tamanho máximo.
         */
         case (mb_strlen($linha . $palavra) + 1 > $length):
           /* 
