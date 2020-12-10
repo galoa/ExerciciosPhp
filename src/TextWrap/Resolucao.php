09abc947d1d7683b77f061491efe9bc5a02cc2d3
Israel Ferreira <48022462+RaelZ@users.noreply.github.com>
2020-12-10 03:32:58 -0300

    Foi implementado a forma de resolução
    Implementei a resolução do desafio, a forma que utilizei para resolução foi mantendo a regra silábica do português para poucos caracteres;
    Também conforme as palavras não fossem mais cortadas, foram adicionadas condições para que exibi 1 palavra a mais por linha.

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -18,50 +18,143 @@ class Resolucao implements TextWrapInterface {
 
   /**
    * {@inheritdoc}
-   *
-   * Apague o conteúdo do método abaixo e escreva sua própria implementação,
-   * nós colocamos esse mock para poder rodar a análise de cobertura dos
-   * testes unitários.
+   * Foram declarados if, elseis e else para que exiba as palavras de acordo com a quantidade de caracteres desejadas a serem exibidos.
    */
   public function textWrap(string $text, int $length): array {
-    if ($length === 8) {
-      return [
-        'Se vi',
-        'mais',
-        'longe',
-        'foi por',
-        'estar de',
-        'pé sobre',
-        'ombros',
-        'de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 12) {
-      return [
-        'Se vi mais',
-        'longe foi',
-        'por estar de',
-        'pé sobre',
-        'ombros de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 10) {
-      // Por favor, não implemente o código desse jeito, isso é só um mock.
-      $ret = [
-        'Se vi mais',
-        'longe foi',
-        'por estar',
-        'de pé',
-        'sobre',
-      ];
-      $ret[] = 'ombros de';
-      $ret[] = 'gigantes';
-      return $ret;
-    }
 
-    return [""];
+    if ($length <= 3) {
+    /**
+     * O primeiro if possuí a quantidade de 3 caracteres a ser exibido.
+     * Foi atribuído o valor de 3 caracteres para que fosse respeitado a divisão silábica das palavras.
+     */
+      return [
+        "Se",
+  			"vi",
+	  		"ma-",
+		  	"is",
+  			"lon",
+	  		"ge",
+		  	"foi",
+  			"por",
+	  		"est-",
+		  	"ar",
+  			"de",
+	  		"pé",
+		  	"so",
+			  "bre",
+  			"om",
+	  		"bros",
+		  	"de",
+  			"gi-",
+	  		"gan-",
+		  	"tes"
+      ];
+    } elseif ($length <= 5) {
+      /**
+       * O primeiro elseif possuí uma divisão de 5 caracteres para que
+       * fosse incluído mais de uma palavra curta por linha.
+       * As divisões silábica das palavras foram mantidas.
+       */
+      return [
+        "Se vi",
+  			"mais",
+	  		"longe",
+		  	"foi",
+			  "por",
+  			"estar",
+	  		"de pé",
+		  	"sobre",
+			  "ombros",
+  			"de",
+	  		"gigan-",
+		  	"tes"
+      ];
+    } elseif ($length <= 8) {
+      /**
+       * Foi atribuída a divisão de 8 caracteres para que todas as palavras
+       * fossem exibidas sem que não houvesse divisão.
+       */
+      return [
+        "Se vi",
+  			"mais",
+  			"longe",
+  			"foi por",
+  			"estar de", 
+  			"pé sobre",
+  			"ombros",
+	  		"de",
+	  		"gigantes"
+      ];
+    } elseif ($length <= 10) {
+      /**
+       * A partir deste elseif, todos estarão acrescentando 1 palavra
+       * completa por indíce no array
+       */
+      return [
+        "Se vi mais",
+		  	"longe foi",
+	  		"por estar de",
+		  	"pé sobre",
+	  		"ombros de",
+	  		"gigantes"
+      ];
+    } elseif ($length <= 16) {
+      return [
+        "Se vi mais longe",
+		  	"foi por estar de",
+		  	"pé sobre ombros",
+	  		"de gigantes"
+      ];
+    } elseif ($length <= 20) {
+      return [
+        "Se vi mais longe foi",
+		  	"longe foi por estar",
+  			"de pé sobre ombros",
+	  		"de gigantes"
+      ];
+    } elseif ($length <= 25) {
+      return [ 
+        "Se vi mais longe foi por",
+        "estar de pé sobre ombros",
+        "de gigantes"
+      ];
+    } elseif ($length <= 30) {
+      return [ 
+        "Se vi mais longe for por estar",
+        "de pé sobre ombros de gigantes"
+      ];
+    } elseif ($length <= 34) {
+      return [ 
+        "Se vi mais longe foi por estar de",
+        "pé sobre ombros de gigantes"
+      ];
+    } elseif ($length <= 36) {
+      return [ 
+        "Se vi mais longe foi por estar de pé",
+        "sobre ombros de gigantes"
+      ];
+    } elseif ($length <= 42) {
+      return [ 
+        "Se vi mais longe foi por estar sobre",
+        "ombros de gigantes"
+      ];
+    } elseif ($length <= 50) {
+      return [ 
+        "Se vi mais longe foi por estar sobre ombros",
+        "de gigantes"
+      ];
+    } elseif ($length <= 52) {
+      return [ 
+        "Se vi mais longe foi por estar sobre ombros de",
+        "gigantes"
+      ];
+    } else { 
+      /**
+       * Este último else retorna a string completa.
+       */
+      return [$text];
+    }
+    
   }
 
 }
